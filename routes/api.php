39533004d9d831cb3f5d1c074a3cfbd62335e3e4
kukohgntg <kukohfr@gmail.com>
2023-12-17 00:21:39 +0700

    add laravel sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HewanController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,4 +18,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix("v1")->group(function () {
     Route::apiResource("hewan", HewanController::class);
+    Route::post('/auth/register', [AuthController::class, 'createUser']);
+    Route::post('/auth/login', [AuthController::class, 'loginUser']);
 });
